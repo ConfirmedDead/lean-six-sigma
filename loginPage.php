2c0303926e3e182451f337534c6a5e1d535f0afe
Logan Baker <25BakerLo@mydacc.org>
2025-04-07 13:48:14 -0400

    Login page setup

---
+++ loginPage.php
@@ -0,0 +1,25 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Login</title>
+</head>
+<div style="width:100%;text-align:center;">
+    <h1>Log In</h1><br>
+</div>
+<table border="0">
+    <tr>
+        <td>Username</td>
+        <td><input type="text" name="username" value="user"></td>
+    </tr>
+    <tr>
+        <td>Password</td>
+        <td><input type="text" name="password" value="pass"></td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td><input type="submit" name="submit" value="Login"></td>
+    </tr>
+</table>
