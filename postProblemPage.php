de4e9eda612addce210fd3c9307b252c3eb6a38f
Gabe Craft <gabecraft0@gmail>
2025-04-10 13:54:33 -0400

    ok

--- postProblemPage.php
+++ postProblemPage.php
@@ -8,16 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
-    <title>Document</title>
 </head>
 <body>
-    <!-- Header -->
-    
-    <!-- Welcome -->
-<div class = "ProblemPage">
+<div class = "problem-page">
     <h1> Jogablogwen Problem Page </h1>
 </div>
     
-
+    
 </body>
 </html>
