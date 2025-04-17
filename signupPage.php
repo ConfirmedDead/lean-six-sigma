ce80e38d952a0c58ddd74742ebd35d9343ef3a27
Logan Baker <25BakerLo@mydacc.org>
2025-04-17 12:45:55 -0400

    Signup tab properly named

--- signupPage.php
+++ signupPage.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignU</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
 </head>
